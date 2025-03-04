c6d4fa6bc5285d2d2276947cfa94f7586e10d086
ASHIQANWARK <ashiqanwar777@gmail.com>
2025-03-04 15:45:21 +0530

    advisor commit

--- src/components/Advisors.jsx
+++ src/components/Advisors.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Briefcase } from "lucide-react";
 
 import ShefinImage from "../assets/images/shefin.jpg";
@@ -11,12 +11,11 @@ const advisors = [
   { name: "Hashim", title: "CAREER CAFE (CEO)", image: hashImage },
   { name: "Abhirami", title: "FLYRAD (CEO)", image: abhiImage },
   { name: "Jino Joseph", title: "FRANCHISIFY (CEO)", image: jinoImage },
-
 ];
 
 const Advisors = () => {
   return (
-    <div className="py-16 bg-gradient-to-b from-[#061428] to-[#0a1931] text-white rounded-tr-full rounded-tl-full rounded-l-full rounded-r-full">
+    <div className="py-16 bg-gradient-to-b from-[#061428] to-[#0a1931] text-white ">
       <div className="container mx-auto px-6">
         {/* Section Title */}
         <h2 className="text-4xl font-bold text-center mb-8 tracking-wide">
@@ -29,18 +28,23 @@ const Advisors = () => {
           {advisors.map((advisor, index) => (
             <div
               key={index}
-              className="bg-white/10 backdrop-blur-lg  rounded-lg p-4 flex flex-col items-center shadow-md"
+              className="bg-white/10 backdrop-blur-lg rounded-lg p-4 flex flex-col items-center shadow-md"
             >
               {/* Advisor Image */}
               <img
                 src={advisor.image}
                 alt={advisor.name}
-                className="w-56 h-56 object-cover rounded-lg "
+                className="w-60 h-60 object-cover rounded-lg"
+                onLoad={(event) => handleImageLoad(index, event)}
               />
               
               {/* Advisor Details */}
-              <h3 className="text-lg font-semibold mt-4 text-white">{advisor.name}</h3>
-              <p className="text-sm text-gray-300 text-center mt-1">{advisor.title}</p>
+              <h3 className="text-lg font-semibold mt-4 text-white">
+                {advisor.name}
+              </h3>
+              <p className="text-sm text-gray-300 text-center mt-1">
+                {advisor.title}
+              </p>
             </div>
           ))}
         </div>
